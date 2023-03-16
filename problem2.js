db5d0e73651ab63107e48af5fe488ec5c4a2dab1
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-16 15:11:25 +0600

    second problem solved

---
+++ problem2.js
@@ -0,0 +1,16 @@
+/* Problem:2  isJavaScriptFile 
+
+ফাংশন নেম দিতে হবে isJavaScriptFile । এই ফাংশনে প্যারামিটার হিসেবে নিবে একটি স্ট্রিং(String) যেটি হবে একটি ফাইল নেম (যেমনঃ ‘index.js’)। যদি এটি জাভাস্ক্রিপ্ট ফাইল হয় তোমাকে true রিটার্ন করতে হবে আর যদি না হয় তাহলে false রিটার্ন করতে হবে ।  
+
+*/
+const myString = 'index.js';
+function isJavaScriptFile(str){
+    if(str.endsWith('js') === true){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+const result = isJavaScriptFile(myString);
+console.log(result);
